4739336afea18bace50397c02003c4d3ebf166ff
batoxa <nbatorin@gmail.com>
2021-04-27 02:01:41 +0300

    51

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,43 +1,17 @@
+import * as axios from 'axios';
 import React from 'react';
 // import UserItem from './UserItem/UserItem';
 import styles from './Users.module.css';
+import userPhoto from '../../assets/images/user.png';
 
 const Users = (props) => {
 
    if (props.users.length === 0) {
-      props.setUsers([
-         {
-            id: 1, fullName: "Wise", avatar: "https://2.bp.blogspot.com/-P1KHVukRJpE/V8Tz6r0eZ8I/AAAAAAACFW0/I0r732kp2wgPSqXBFl0i_DkCndzrY5rLACLcB/s320/Wise.png",
-            location: { city: "Moscow", country: "Russia" },
-            status: "Pro",
-            followed: true
-         },
-         {
-            id: 2, fullName: "Total", avatar: "https://media.istockphoto.com/vectors/cow-icon-vector-id652692584",
-            location: { city: "Klimovsk", country: "Russia" },
-            status: "Junior",
-            followed: true
-         },
-         {
-            id: 3, fullName: "ZloySky", avatar: "https://yt3.ggpht.com/ytc/AAUvwnjN-7ImWo3a5BUbV-LRQrYFGTjHG_vctOYz4NCf=s900-c-k-c0x00ffffff-no-rj",
-            location: { city: "Saratov", country: "Russia" },
-            status: "Jude",
-            followed: false
-         },
-         {
-            id: 4, fullName: "Skawolf", avatar: "https://yt3.ggpht.com/a/AATXAJxdL0vOU2JGwmuP9TlOhXlCf8Pm9LHVk18YE1Xo=s900-c-k-c0x00ffffff-no-rj",
-            location: { city: "Ryazan", country: "Russia" },
-            status: "Newbee",
-            followed: false
-         },
-         {
-            id: 5, fullName: "Tendo", avatar: "https://yt3.ggpht.com/ytc/AAUvwnhHlQn5yh79uOtvDHMJmYXZ3sXnqycPw4SHFMRB=s900-c-k-c0x00ffffff-no-rj",
-            location: { city: "Komunarka", country: "Russia" },
-            status: "Middle",
-            followed: false
-         }
-      ]
-      )
+
+      axios.get("https://social-network.samuraijs.com/api/1.0/users")
+         .then(response => {
+            props.setUsers(response.data.items)
+         })
    }
 
    // followUser = (userId) => () => props.followUser(userId)
@@ -45,17 +19,17 @@ const Users = (props) => {
    const userElements = props.users.map(user =>
       <div className={styles.itemwrapper}>
          <div className={styles.avatar}>
-            <img src={user.avatar} alt='avatar' />
+            <img src={user.photos.small != null ? user.photos.small : userPhoto} alt='avatar' />
          </div>
          <div className={styles.description}>
             <div className={styles.fullname}>
-               {user.fullName}
+               {user.name}
             </div>
             <div className={styles.status}>
-               {user.status}
+               {user.status === null? "user.status": user.status}
             </div>
             <div className={styles.location}>
-               {user.location.city}, {user.location.country}
+               {"user.location.city"}, {"user.location.country"}
             </div>
          </div>
          <div className={styles.follow}>
